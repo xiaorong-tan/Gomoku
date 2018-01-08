cf42e18c2392dd8a8c08d91bc90e241cf8f69c47
xiaorong-tan <tanxiaorong104@hotmail.com>
2018-01-08 19:22:57 +0000

    Update Robot.java

--- Robot.java
+++ Robot.java
@@ -2,8 +2,8 @@ public class Robot {
     private Board bd;
     private int INFINITY = 1000000;
     private int movex, movey;
-    private int level;// 深度
-    private int node;// 每层结点
+    private int level;// depth
+    private int node;
 
     public Robot(Board bd, int level, int node) {
         this.bd = bd;
@@ -12,21 +12,21 @@ public class Robot {
     }
 
 
-    // 估值函数+搜索树
-    public int[] findTreeBestStep() {
+    
+    public int[] findBestStep() {
         alpha_beta(0, bd, -INFINITY, INFINITY);
         int[] result = { movex, movey };
         return result;
     }
 
-    // alpha-beta剪枝搜索算法
+    // alpha-beta algorithm
     public int alpha_beta(int depth, Board board, int alpha, int beta) {
         if (depth == level || board.judge() != 0) {
             Stone[] sorted = board.getSorted();
             Stone move = board.getBoard()[sorted[0].x][sorted[0].y];
-            return move.getSum();// 局面估分
+            return move.getSum();
         }
-        // 对局面下得分最高的几个点进行拓展
+        // put a stone based on the high score
         Board temp = new Board(board);
         Stone[] sorted = temp.getSorted();
         int score;
@@ -34,19 +34,19 @@ public class Robot {
             int x = sorted[i].x;
             int y = sorted[i].y;
 
-            // 走这个走法
+            // put a stone on x, y
             if (!temp.moveStone(x, y))
                 continue;
             if (sorted[i].getOffense() >= Board.Level.LIVE_4.score) {
-                //System.out.println("我们快要赢啦！");
+                
                 score = INFINITY + 1;
             } else if (sorted[i].getDefense() >= Board.Level.LIVE_4.score) {
-                //System.out.println("对方快要赢啦！");
+                
                 score = INFINITY;
             } else {
                 score = alpha_beta(depth + 1, temp, alpha, beta);
             }
-            temp = new Board(board);// 撤消这个走法
+            temp = new Board(board);// undo current move
 
             if (depth % 2 == 0) {// MAX
                 if (score > alpha) {
@@ -57,8 +57,7 @@ public class Robot {
                     }
                 }
                 if (alpha >= beta) {
-                    score = alpha;
-                    // System.out.println(" beta剪枝");
+                    score = alpha;                    
                     return score;
                 }
             } else {// MIN
@@ -66,8 +65,7 @@ public class Robot {
                     beta = score;
                 }
                 if (alpha >= beta) {
-                    score = beta;
-                    // System.out.println(" alpha剪枝");
+                    score = beta;                    
                     return score;
                 }
             }
